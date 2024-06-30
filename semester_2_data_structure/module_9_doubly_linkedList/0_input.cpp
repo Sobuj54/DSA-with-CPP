8cd2b9c480f398a16bf2df0b2988cbf2efc31834
soubj <sobujahmed124@gmail.com>
2024-06-30 15:40:25 +0600

    take input in doubly linked list

---
+++ semester_2_data_structure/module_9_doubly_linkedList/0_input.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *prev;
+    Node *next;
+
+    Node(int value)
+    {
+        this->value = value;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (tail == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+
+void print(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->value << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+void print_reverse(Node *tail)
+{
+    while (tail != NULL)
+    {
+        cout << tail->value << " ";
+        tail = tail->prev;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (cin >> val && val != -1)
+    {
+        insert_at_tail(head, tail, val);
+    }
+    print(head);
+    print_reverse(tail);
+    return 0;
+}
