b7e865d561da7a7f49e5035e27c2e84ae5d69555
soubj <sobujahmed124@gmail.com>
2024-08-24 13:20:33 +0600

    count leaf nodes

---
+++ semester_2_data_structure/module_18_operations_on_binary_tree/4_count_leaf_nodes.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node *inputTree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+
+    while (!q.empty())
+    {
+        Node *first = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft;
+        Node *myRight;
+        l == -1 ? myLeft = NULL : myLeft = new Node(l);
+        r == -1 ? myRight = NULL : myRight = new Node(r);
+        first->left = myLeft;
+        first->right = myRight;
+
+        if (first->left)
+            q.push(first->left);
+        if (first->right)
+            q.push(first->right);
+    }
+
+    return root;
+}
+
+int count_leaf(Node *root)
+{
+    if (root == NULL)
+        return 0;
+    if (!root->left && !root->right)
+        return 1;
+    else
+    {
+        int l = count_leaf(root->left);
+        int r = count_leaf(root->right);
+        return l + r;
+    }
+}
+
+int main()
+{
+    Node *root = inputTree();
+    cout << count_leaf(root) << endl;
+    return 0;
+}
