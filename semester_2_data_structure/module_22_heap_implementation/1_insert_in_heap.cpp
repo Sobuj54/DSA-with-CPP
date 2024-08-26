3193344924fbafc788c18320b328b82312951a85
soubj <sobujahmed124@gmail.com>
2024-08-26 19:59:54 +0600

    insert_in max heap

---
+++ semester_2_data_structure/module_22_heap_implementation/1_insert_in_heap.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+        int currentIndex = v.size() - 1;
+
+        while (currentIndex != 0)
+        {
+            int parentIndex = (currentIndex - 1) / 2;
+            if (v[parentIndex] < v[currentIndex])
+                swap(v[parentIndex], v[currentIndex]);
+            else
+                break;
+            currentIndex = parentIndex;
+        }
+    }
+    for (int val : v)
+    {
+        cout << val << " ";
+    }
+    return 0;
+}
