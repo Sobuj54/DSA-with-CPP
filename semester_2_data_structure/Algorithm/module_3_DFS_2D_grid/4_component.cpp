3eb64f228696620c3955e5ce507a10b22f5dc1d9
soubj <sobujahmed124@gmail.com>
2024-09-04 20:26:37 +0600

    finding number of components

---
+++ semester_2_data_structure/Algorithm/module_3_DFS_2D_grid/4_component.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5;
+vector<int> v[N];
+bool visited[N];
+
+void dfs(int src)
+{
+    cout << src << endl;
+    visited[src] = true;
+    for (int child : v[src])
+    {
+        if (!visited[child])
+        {
+            dfs(child);
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+
+    memset(visited, false, sizeof(visited));
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i])
+        {
+            dfs(i);
+            count++;
+        }
+    }
+    cout << "component : " << count << endl;
+    return 0;
+}
