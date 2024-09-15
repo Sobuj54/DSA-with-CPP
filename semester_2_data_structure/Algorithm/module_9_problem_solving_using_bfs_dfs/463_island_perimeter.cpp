253358e8c5fe20c850f32766d915f09b474609fd
soubj <sobujahmed124@gmail.com>
2024-09-15 19:37:26 +0600

    find perimeter of islan in 2d grid

---
+++ semester_2_data_structure/Algorithm/module_9_problem_solving_using_bfs_dfs/463_island_perimeter.cpp
@@ -0,0 +1,59 @@
+// https://leetcode.com/problems/island-perimeter/description/
+
+class Solution
+{
+public:
+    vector<pair<int, int>> p = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+    bool visited[105][105];
+    int row, col, perimeter;
+
+    bool isValid(int i, int j)
+    {
+        if (i >= 0 && i < row && j >= 0 && j < col)
+            return true;
+        else
+            return false;
+    }
+
+    void dfs(int si, int sj, vector<vector<int>> &grid)
+    {
+        visited[si][sj] = true;
+        for (int i = 0; i < 4; i++)
+        {
+            int ci = si + p[i].first;
+            int cj = sj + p[i].second;
+            if (isValid(ci, cj))
+            {
+                if (grid[ci][cj] == 0)
+                    perimeter++;
+                if (!visited[ci][cj] && grid[ci][cj] == 1)
+                {
+                    dfs(ci, cj, grid);
+                }
+            }
+            else
+            {
+                perimeter++;
+            }
+        }
+    }
+
+    int islandPerimeter(vector<vector<int>> &grid)
+    {
+        memset(visited, false, sizeof(visited));
+        row = grid.size();
+        col = grid[0].size();
+        perimeter = 0;
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (!visited[i][j] && grid[i][j] == 1)
+                {
+                    dfs(i, j, grid);
+                }
+            }
+        }
+        return perimeter;
+    }
+};
