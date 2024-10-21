15e53b0d01010a71e2fd2330ed6d335cb634b5a8
soubj <sobujahmed124@gmail.com>
2024-10-21 23:16:40 +0600

    printing lcs and lcs bottom up mistake fixed

--- semester_2_data_structure_and_algorithm/Algorithm/module_21_longest_common_subsequence/3_printing_LCS.cpp
+++ semester_2_data_structure_and_algorithm/Algorithm/module_21_longest_common_subsequence/3_printing_LCS.cpp
@@ -29,6 +29,25 @@ int main()
         }
     }
 
-    cout << dp[n][m];
+    int i=n, j=m;
+    string ans;
+    while(i!=0 && j!=0){
+        if(a[i-1] == b[j-1]){
+            ans += a[i-1];
+            i--;
+            j--;
+        }
+        else{
+            if(dp[i][j-1] > dp[i-1][j]){
+                j--;
+            }
+            else{
+                i--;
+            }
+        }
+    }
+
+    reverse(ans.begin(), ans.end());
+    cout<<ans;
     return 0;
 }
