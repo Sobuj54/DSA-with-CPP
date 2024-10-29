ee779557ca5f0b5fef1a8580bc3513407c960564
soubj <sobujahmed124@gmail.com>
2024-10-29 19:09:26 +0600

    misha and changing handles

---
+++ problems solved/week_1/day_4/1_misha_and_changing_handles.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    map<string, pair<string, string>> mp;
+
+    string old, cur;
+    cin >> old >> cur;
+
+    mp[cur] = {old, cur};
+    for (int i = 1; i < n; i++)
+    {
+        cin >> old >> cur;
+        auto it = mp.find(old);
+        if (it != mp.end())
+        {
+            string prevOld = mp[old].first;
+            mp[cur] = {prevOld, cur};
+            mp.erase(it);
+        }
+        else
+        {
+
+            mp[cur] = {old, cur};
+        }
+    }
+
+    cout << mp.size() << "\n";
+    for (auto x : mp)
+    {
+        cout << x.second.first << " " << x.second.second << "\n";
+    }
+    return 0;
+}
