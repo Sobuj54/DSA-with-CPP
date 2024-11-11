fc62f8b33f559852ec9e7b85f3538649c453414a
soubj <sobujahmed124@gmail.com>
2024-11-11 23:30:03 +0600

    remove prefix problem solved

--- problems solved/week_3/day_2/F_remove_prefix.cpp
+++ problems solved/week_3/day_2/F_remove_prefix.cpp
@@ -6,5 +6,44 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<pair<int, bool>> v(n);
+        set<int> s;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            v[i] = {x, false};
+        }
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (i == n - 1)
+            {
+                s.insert(v[i].first);
+            }
+            else if (s.find(v[i].first) == s.end() && v[i + 1].second == false)
+            {
+                s.insert(v[i].first);
+            }
+            else
+            {
+                v[i].second = true;
+            }
+        }
+
+        int count = 0;
+        for (auto [x, y] : v)
+        {
+            if (y)
+                count++;
+        }
+        cout << count << "\n";
+    }
     return 0;
 }
