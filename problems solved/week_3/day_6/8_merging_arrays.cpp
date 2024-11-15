0ef90c77192c3f3bc95571a7711ee01ec5b827de
soubj <sobujahmed124@gmail.com>
2024-11-16 00:11:45 +0600

    fix: merging arrays

--- problems solved/week_3/day_6/8_merging_arrays.cpp
+++ problems solved/week_3/day_6/8_merging_arrays.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
 
 int main()
@@ -9,38 +8,46 @@ int main()
 
     int n, m;
     cin >> n >> m;
-    vector<ll> a(n), b(m);
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-    for (int i = 0; i < m; i++)
-        cin >> b[i];
+    vector<int> a(n), b(m);
 
-    int l = 0, r = 0, x = max(n, m);
-    while (l < x && r < x)
+    for (int i = 0; i < n; i++)
     {
-        if (l < n && r < m && a[l] <= b[r])
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        cin >> b[i];
+    }
+
+    int i = 0, j = 0;
+    while (i < n && j < m)
+    {
+        if (a[i] <= b[j])
         {
-            cout << a[l] << " ";
-            l++;
-        }
-        else if (r < m && l < n && b[r] < a[l])
-        {
-            cout << b[r] << " ";
-            r++;
+            cout << a[i] << " ";
+            i++;
         }
         else
         {
-            if (max(n, m) == n)
-            {
-                cout << a[l] << " ";
-                l++;
-            }
-            else
-            {
-                cout << b[r] << " ";
-                r++;
-            }
+            cout << b[j] << " ";
+            j++;
         }
     }
+
+    while (i < n)
+    {
+        cout << a[i] << " ";
+        i++;
+    }
+
+    while (j < m)
+    {
+        cout << b[j] << " ";
+        j++;
+    }
+
+    cout << endl;
+
     return 0;
 }
