e54a19324d39441e8231ba32f48764eaf8a25a54
soubj <sobujahmed124@gmail.com>
2025-03-05 22:46:49 +0600

    day 3 target fulfilled

---
+++ problems solved/week_18/day_3/E - Zhan's Blender.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x, y;
+        cin >> n >> x >> y;
+        x = min(x, y);
+        cout << (n + x - 1) / x << endl;
+    }
+    return 0;
+}
